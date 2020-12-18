41987601ae85121321442c342a41df1ecec99878
Bokoko <kamabokoman1042@gmail.com>
2020-12-18 23:16:35 +0900

    色を修正

--- components/artwork/js/variable.js
+++ components/artwork/js/variable.js
@@ -1,9 +1,10 @@
 export const colors = {
   white: 0xf5f5f5,
+  beige: 0xf0f0e5,
   pink: 0xfad8d6,
   gray: 0x2c2d3e,
   black: 0x0e1428,
-  blue: 0x1c7b9b,
-  maroon: 0xaf3b6e,
-  green: 0x6baa75,
+  blue: 0x4caabd,
+  mint: 0x95dace,
+  red: 0xdf7c87,
 };
